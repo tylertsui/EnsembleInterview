293078e6e0b84ae434f0fc5b62b3d0e1e8c0fb25
Tyler T <tylertsui@outlook.com>
2021-06-27 14:09:41 -0700

    added accidentally deleted code back

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router";
 
 import { film_link, world_link, specie_link, vehicle_link, starship_link } from "../strings/String";
+import LinkBlock from "../common/LinkBlock";
 import FilmBlockList from "../common/FilmBlockList";
 import LinkBlockList from "../common/LinkBlockList";
 
